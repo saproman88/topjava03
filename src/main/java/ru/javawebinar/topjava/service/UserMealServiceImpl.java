03055be73ce3c504e29d46d6e78a86e3d0db41c3
saproman88 <trdv03pt>
2015-06-18 15:56:41 +0300

    HW2 release

--- src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
+import ru.javawebinar.topjava.util.exception.ExceptionUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
@@ -21,26 +22,25 @@ public class UserMealServiceImpl implements UserMealService {
     @Override
     public UserMeal save(UserMeal userMeal)
     {
-        return null;
+        return repository.save(userMeal);
     }
 
     @Override
     public void delete(int id) throws NotFoundException
     {
-
+        ExceptionUtil.check(repository.delete(id), id);
     }
 
     @Override
     public UserMeal get(int id) throws NotFoundException
     {
-        return null;
+        return ExceptionUtil.check(repository.get(id), id);
     }
 
     @Override
     public void update(UserMeal userMeal) throws NotFoundException
     {
-
-
+        ExceptionUtil.check(repository.save(userMeal), userMeal.getId());
     }
 
     @Override
